d1044316d6efc5d7fcf350517e1e5bd6c34656f6
jonaszb <jonaszbil@gmail.com>
2022-05-13 20:38:33 +0100

    Extend page objects

--- pageObjects/index.py
+++ pageObjects/index.py
@@ -1,8 +1,12 @@
 from pageObjects.inventory_page import InventoryPage
 from pageObjects.login_page import LoginPage
 from pageObjects.menu import Menu
+from pageObjects.base import Base
 
 class PageObjects:
+
+    def base(page):
+        return Base(page)
 
     def login_page(page):
         return LoginPage(page)
--- pageObjects/menu.py
+++ pageObjects/menu.py
@@ -5,3 +5,9 @@ class Menu(Base):
     def __init__(self, page):
         super().__init__(page)
         self.page = page
+        self.root_element = page.locator("div.bm-menu-wrap")
+        self.menu_items = self.root_element.locator("a.menu-item")
+        self.close_icon = self.root_element.locator("#react-burger-cross-btn")
+
+    def get_item_with_text(self, text):
+       return self.menu_items.filter(has_text=text) 
